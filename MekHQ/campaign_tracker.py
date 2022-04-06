251cfbdb3ec0cccaa1247fb4a0f96d50b9a5fab4
Maarten Broekman <maarten@broekman.us>
2022-04-05 20:21:01 -0400

    mjbroekman - fixed import and class instantiation

--- MekHQ/campaign_tracker.py
+++ MekHQ/campaign_tracker.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import campaign
+from campaign import campaign
 
 if __name__ == "__main__":
-    campaign = Campaign(sys.argv[1])
-    campaign.readCampaign()
+    my_campaign = campaign.Campaign(sys.argv[1])
+    my_campaign.readCampaign()
 
 
