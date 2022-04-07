ab350e3de2fe39f1a356861c8466b19623f76777
Maarten Broekman <maarten@broekman.us>
2022-04-06 21:32:17 -0400

    mjbroekman - adding license

--- MekHQ/PyMek/setup.py
+++ MekHQ/PyMek/setup.py
@@ -5,5 +5,6 @@ setuptools.setup(
     name="pymek",
     version="1.0.0",
     description="Tool for interrogating MekHQ campaign saves",
+    license="GPL-3.0 - https://opensource.org/licenses/GPL-3.0",
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'})
