5b3bd2374cd75a0bec0d30f97063ef34a337c732
Maarten Broekman <maarten@broekman.us>
2022-04-06 20:33:40 -0400

    mjbroekman - setup.py file

--- MekHQ/PyMek/setup.py
+++ MekHQ/PyMek/setup.py
@@ -1,1 +1,9 @@
 # setup.py file for pymek package
+import setuptools
+
+setuptools.setup(
+    name="pymek",
+    version="1.0.0",
+    description="Tool for interrogating MekHQ campaign saves",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'})
