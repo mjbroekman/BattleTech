0b17750f5a8417e3fd219e0fb45327b504e9ef10
Maarten Broekman <maarten@broekman.us>
2025-05-02 16:38:18 -0400

    mjbroekman - add in new imports from selenium that may be useful in getting this script working again

--- MWO/mwo_stats.py
+++ MWO/mwo_stats.py
@@ -27,6 +27,9 @@ from getpass import getpass
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from urllib3.exceptions import NewConnectionError
 from urllib3.exceptions import MaxRetryError
 from urllib3.exceptions import ProtocolError
